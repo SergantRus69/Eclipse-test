bc5948599d6fd52a009e3441ec16474942ebbad8
Sergey <Sergey@mbp-sergey>
2018-07-15 14:16:36 +0300

    My tree string

--- github-test-project/src/ru/fkp/githubtest/Main.java
+++ github-test-project/src/ru/fkp/githubtest/Main.java
@@ -3,7 +3,8 @@ package ru.fkp.githubtest;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("DFDSFSD!!!!!sdasdasd!!!!!");
+		System.out.println("DFDSFSD!!!sdasdasd!!!!!!!!!");
+		System.out.println("DFDSFSD!!!!!!!!!!");
 
 	}
 
